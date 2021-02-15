5d3ef357e5698eedb424a4081eaeb0ecfd2ea8f9
haraphat01 <haraphat@gmail.com>
2021-02-15 08:41:51 +0100

    Worked on the display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,2 +1,18 @@
-const Display = () => <h1>Display</h1>;
+import PropTypes from 'prop-types';
+
+const Display = props => {
+  const { result } = props;
+  return (
+    <>
+      {result}
+    </>
+  );
+};
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
+};
 export default Display;
